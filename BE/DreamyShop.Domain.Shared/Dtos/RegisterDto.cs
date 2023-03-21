857b4a044190792b115560750a1b72722273927a
linh.nguyen22 <linh.nguyen22@D-OS3-LINHNM22>
2023-03-21 17:42:10 +0700

    Coding Authentication

---
+++ BE/DreamyShop.Domain.Shared/Dtos/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamyShop.Domain.Shared.Dtos
+{
+    public class RegisterDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool GenderType { get; set; }
+        public string Phone { get; set; }
+        public string Token { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
