5aa5ff73559a5423cdf3ea12b3616da75c6a6043
LikWiKi <nguyenmanhlinh6621@gmail.com>
2023-05-10 00:37:40 +0700

    Coding ProductVariant

--- FE/src/app/catalog/product/variation-edit-item/variation-edit-item.component.ts
+++ FE/src/app/catalog/product/variation-edit-item/variation-edit-item.component.ts
@@ -1,26 +1,43 @@
-import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-variation-edit-item',
   templateUrl: './variation-edit-item.component.html',
   styleUrls: ['./variation-edit-item.component.scss'],
 })
-export class VariationEditItemComponent {
+export class VariationEditItemComponent implements OnInit {
   @Input() productOptionKey: string = '';
   @Input() productOptionValue: string[] = [];
   @Input() indexProductOption: number = 1;
 
-  inputs: { value: string }[] = [{ value: '' }];
-  inputValues: string[] = [''];
+  containerVariants: string[] = [''];
+  indexProductVariant: number = 1;
+  inputs: { value: string }[][] = [[{ value: '' }]];
+  containerVariantCount: number = 0;
 
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  constructor() {}
+
+  ngOnInit(): void {}
 
   onInputFocus() {
+    let index = this.indexProductVariant - 1;
     if (
-      this.inputs.length === 0 ||
-      this.inputs[this.inputs.length - 1].value !== ''
+      this.inputs[index].length === 0 ||
+      this.inputs[index][this.inputs[index].length - 1].value !== ''
     ) {
-      this.inputs.push({ value: '' });
+      this.inputs[index].push({ value: '' });
+      console.log('this.inputs22222222222 :>> ', this.inputs[index]);
     }
+  }
+
+  addProductVariant() {
+    this.containerVariants.push('');
+    this.inputs.push([{ value: '' }]);
+    this.indexProductVariant++;
+    this.containerVariantCount++;
+  }
+
+  onDeleteClassify() {
+    this.inputs[this.indexProductVariant - 1].splice(-1);
   }
 }
