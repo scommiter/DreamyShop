14fc86601f09bd03f6388bc8b3fa061291f950f2
LikWiKi <nguyenmanhlinh6621@gmail.com>
2023-06-18 00:14:32 +0700

    Fix Product Paging API

--- BE/DreamyShop.Logic/Product/ProductLogic.cs
+++ BE/DreamyShop.Logic/Product/ProductLogic.cs
@@ -30,17 +30,172 @@ namespace DreamyShop.Logic.Product
             _mapper = mapper;
         }
 
+        //public async Task<ApiResult<PageResult<ProductDto>>> GetAllProductPaging(PagingRequest pagingRequest)
+        //{
+        //    var attributes = _repository.Attribute.GetAll().ToList();
+        //    var attributeValues = _repository.ProductAttribute.GetAll().ToList();
+        //    var attributeProducts = _context.Attributes.Join(_context.ProductAttributes,
+        //                                a => a.Id,
+        //                                b => b.AttributeId,
+        //                                (a, b) => new
+        //                                {
+        //                                    AttributeName = a.Name,
+        //                                    ProductId = b.ProductId
+        //                                });
+        //    var query = (from p in _context.Products
+        //                join m in _context.Manufacturers on p.ManufacturerId equals m.Id
+        //                join c in _context.ProductCategories on p.CategoryId equals c.Id
+        //                join pv in _context.ProductVariants on p.Id equals pv.ProductId into pvN
+        //                from pv in pvN.DefaultIfEmpty()
+        //                join ip in _context.ImageProducts on p.Id equals ip.ProductId into ipvN
+        //                from ip in ipvN.DefaultIfEmpty()
+        //                join pvv in _context.ProductVariantValues on pv.Id equals pvv.ProductVariantId into pvvN
+        //                from pvv in pvvN.DefaultIfEmpty()
+        //                join pav in _context.ProductAttributeValues on pvv.ProductAttributeValueId equals pav.Id into pavN
+        //                from pav in pavN.DefaultIfEmpty()
+        //                orderby p.DateCreated
+        //                select new
+        //                {
+        //                    Product = p,
+        //                    ManufacturerName = m.Name,
+        //                    CategoryName = c.Name,
+        //                    pvv,
+        //                    pv,
+        //                    pav,
+        //                    ip
+        //                });
+
+        //    var groupedQuery = query.GroupBy(item => item.Product.Id)
+        //                            .Select(group => new
+        //                            {
+        //                                ProductId = group.Key,
+        //                                Product = group.FirstOrDefault().Product,
+        //                                ManufacturerName = group.Select(item => item.ManufacturerName).FirstOrDefault(),
+        //                                CategoryName = group.Select(item => item.CategoryName).FirstOrDefault(),
+        //                                pvv = group.Where(item => item.pvv != null).Select(item => item.pvv).Distinct(),
+        //                                pv = group.Where(item => item.pv != null).Select(item => item.pv).Distinct(),
+        //                                pav = group.Where(item => item.pav != null).Select(item => item.pav).Distinct(),
+        //                                ip = group.Where(item => item.ip != null).Select(item => item.ip).Distinct()
+        //                            }).Skip((pagingRequest.Page - 1) * pagingRequest.Limit).Take(pagingRequest.Limit);
+
+        //    var productsPaging = await groupedQuery.ToListAsync();
+        //    var totalCount = await _context.Products.CountAsync();
+        //    var productDtos = productsPaging.Select(x => new ProductDto
+        //    {
+        //                            Id = x.ProductId,
+        //                            Name = x.Product.Name,
+        //                            Code = x.Product.Code,
+        //                            ThumbnailPictures = x.ip.GroupBy(p => p.ProductId).Select(pt => pt.Select(ptt => ptt.Path ?? "").FirstOrDefault()).ToList(),
+        //                            ProductType = x.Product.ProductType,
+        //                            CategoryName = x.CategoryName ?? "",
+        //                            ManufacturerName = x.ManufacturerName ?? "",
+        //                            Description = x.Product.Description ?? "",
+        //                            IsActive = x.Product.IsActive,
+        //                            IsVisibility = x.Product.IsVisibility,
+        //                            DateCreated = x.Product.DateCreated,
+        //                            DateUpdated = x.Product.DateUpdated,
+        //                            OptionNames = attributeProducts.Where(a => a.ProductId == x.ProductId)?.Select(a => a.AttributeName).ToList(),
+        //                            ProductAttributeDisplayDtos = x.pv.Select(pv => new ProductAttributeDisplayDto
+        //                            {
+        //                                                                AttributeNames = x.pav.Where(e => e.ProductId == x.ProductId)
+        //                                                                                 .Where(p => (x.pvv.GroupBy(pvv => pvv.ProductVariantId).Where(pc => pc.Key == pv.Id).FirstOrDefault().Select(pi => pi.ProductAttributeValueId)).Contains(p.Id))
+        //                                                                                 .Select(e => e.Value).ToList(),                   
+        //                                                                SKU = pv.SKU,
+        //                                                                Quantity = pv.Quantity,
+        //                                                                Price = pv.Price,
+        //                                                                Image = pv.ThumbnailPicture
+        //                                                            }).OrderByDescending(a => a.AttributeNames.FirstOrDefault()).ToList()
+        //                            }).ToList();
+
+        //    var pageResult = new PageResult<ProductDto>()
+        //    {
+        //        Items = productDtos,
+        //        Totals = totalCount
+        //    };
+        //    return new ApiSuccessResult<PageResult<ProductDto>>(pageResult);
+        //}
+
         public async Task<ApiResult<PageResult<ProductDto>>> GetAllProductPaging(PagingRequest pagingRequest)
         {
-            var productDtos = GetAllProductDto().Result;
+            var attributes = _repository.Attribute.GetAll().ToList();
+            var attributeValues = _repository.ProductAttribute.GetAll().ToList();
+            var attributeProducts = _context.Attributes.Join(_context.ProductAttributes,
+                                        a => a.Id,
+                                        b => b.AttributeId,
+                                        (a, b) => new
+                                        {
+                                            AttributeName = a.Name,
+                                            ProductId = b.ProductId
+                                        });
+
+            var query = (from p in _context.Products.OrderByDescending(p => p.DateCreated).Skip((pagingRequest.Page - 1) * pagingRequest.Limit).Take(pagingRequest.Limit)
+                         join m in _context.Manufacturers on p.ManufacturerId equals m.Id
+                         join c in _context.ProductCategories on p.CategoryId equals c.Id
+                         join pv in _context.ProductVariants on p.Id equals pv.ProductId into pvN
+                         from pv in pvN.DefaultIfEmpty()
+                         join ip in _context.ImageProducts on p.Id equals ip.ProductId into ipvN
+                         from ip in ipvN.DefaultIfEmpty()
+                         join pvv in _context.ProductVariantValues on pv.Id equals pvv.ProductVariantId into pvvN
+                         from pvv in pvvN.DefaultIfEmpty()
+                         join pav in _context.ProductAttributeValues on pvv.ProductAttributeValueId equals pav.Id into pavN
+                         from pav in pavN.DefaultIfEmpty()
+                         select new
+                         {
+                             Product = p,
+                             ManufacturerName = m.Name,
+                             CategoryName = c.Name,
+                             pvv,
+                             pv,
+                             pav,
+                             ip
+                         });
+
+            var groupedQuery = query.GroupBy(item => item.Product.Id)
+                                    .Select(group => new
+                                    {
+                                        ProductId = group.Key,
+                                        Product = group.FirstOrDefault().Product,
+                                        ManufacturerName = group.Select(item => item.ManufacturerName).FirstOrDefault(),
+                                        CategoryName = group.Select(item => item.CategoryName).FirstOrDefault(),
+                                        pvv = group.Where(item => item.pvv != null).Select(item => item.pvv).Distinct(),
+                                        pv = group.Where(item => item.pv != null).Select(item => item.pv).Distinct(),
+                                        pav = group.Where(item => item.pav != null).Select(item => item.pav).Distinct(),
+                                        ip = group.Where(item => item.ip != null).Select(item => item.ip).Distinct()
+                                    });
+
+            var productsPaging = await groupedQuery.ToListAsync();
+            var totalCount = await _context.Products.CountAsync();
+            var productDtos = productsPaging.Select(x => new ProductDto
+            {
+                Id = x.ProductId,
+                Name = x.Product.Name,
+                Code = x.Product.Code,
+                ThumbnailPictures = x.ip.GroupBy(p => p.ProductId).Select(pt => pt.Select(ptt => ptt.Path ?? "").FirstOrDefault()).ToList(),
+                ProductType = x.Product.ProductType,
+                CategoryName = x.CategoryName ?? "",
+                ManufacturerName = x.ManufacturerName ?? "",
+                Description = x.Product.Description ?? "",
+                IsActive = x.Product.IsActive,
+                IsVisibility = x.Product.IsVisibility,
+                DateCreated = x.Product.DateCreated,
+                DateUpdated = x.Product.DateUpdated,
+                OptionNames = attributeProducts.Where(a => a.ProductId == x.ProductId)?.Select(a => a.AttributeName).ToList(),
+                ProductAttributeDisplayDtos = x.pv.Select(pv => new ProductAttributeDisplayDto
+                {
+                    AttributeNames = x.pav.Where(e => e.ProductId == x.ProductId)
+                                                                     .Where(p => (x.pvv.GroupBy(pvv => pvv.ProductVariantId).Where(pc => pc.Key == pv.Id).FirstOrDefault().Select(pi => pi.ProductAttributeValueId)).Contains(p.Id))
+                                                                     .Select(e => e.Value).ToList(),
+                    SKU = pv.SKU,
+                    Quantity = pv.Quantity,
+                    Price = pv.Price,
+                    Image = pv.ThumbnailPicture
+                }).OrderByDescending(a => a.AttributeNames.FirstOrDefault()).ToList()
+            }).OrderByDescending(p => p.DateCreated).ToList();
 
             var pageResult = new PageResult<ProductDto>()
             {
-                Items = productDtos.OrderByDescending(p => p.DateCreated)
-                    .Skip((pagingRequest.Page - 1) * pagingRequest.Limit)
-                                .Take(pagingRequest.Limit)
-                                .ToList(),
-                Totals = productDtos.Count()
+                Items = productDtos,
+                Totals = totalCount
             };
             return new ApiSuccessResult<PageResult<ProductDto>>(pageResult);
         }
@@ -550,7 +705,8 @@ namespace DreamyShop.Logic.Product
                 product.IsVisibility = productUpdateDto.IsVisibility ?? true;
             }
 
-            if (productUpdateDto.Images != null)
+            productUpdateDto.Images.RemoveAll(p => p == "");
+            if (productUpdateDto.Images.Count > 0)
             {
                 UploadProductImages(productUpdateDto.Images, id, true);
             }
