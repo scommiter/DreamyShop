ce1d9920e231c727d8efde240503a28db8fb9c30
LUPIN <nguyenmanhlinh6621@gmail.com>
2023-07-22 16:06:59 +0700

    update HomePage FE

--- DREAMYMVC/wwwroot/js/site.js
+++ DREAMYMVC/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
